1ac2178065dc2f1fd5727a015e1725959ec0d464
kkaran13 <kmodi7542@gmail.com>
2024-05-26 22:37:32 +0530

    commited

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-a7s6.vercel.app", {
 				query: {
 					userId: authUser._id,
 				},
